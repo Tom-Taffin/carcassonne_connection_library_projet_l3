fd65caffb2ac6769b4cfea753caa67b1e4e7f063
Samuel DEMON <samuel.demon.etu@univ-lille.fr>
2026-01-28 09:12:42 +0100

    feat: adding Place command

--- src/main/java/l3s6/projet/star/interaction/command/EnterCommand.java
+++ src/main/java/l3s6/projet/star/interaction/command/EnterCommand.java
@@ -1,17 +1,16 @@
 package l3s6.projet.star.interaction.command;
 
-
 import l3s6.projet.star.interaction.view.AbstractView;
 
 import java.util.List;
 
-public class EnterCommand extends AbstractCommand<T extends AbstractView> {
+public class EnterCommand<V extends AbstractView> extends AbstractCommand<V> {
 
      public String build(String id, Object... params){
         return id + " ENTERS";
      }
 
-     public void execute(List<String> parts, T view){
+     public void execute(List<String> parts, V view){
          view.updateOnEnter(parts.get(0));
      }
 }
---
+++ src/main/java/l3s6/projet/star/interaction/command/PlaceCommand.java
@@ -0,0 +1,20 @@
+package l3s6.projet.star.interaction.command;
+
+import l3s6.projet.star.interaction.view.AbstractView;
+
+import java.util.List;
+
+public class PlaceCommand<V extends AbstractView> extends AbstractCommand<V> {
+
+    public String build(String id, Object... params){
+        return String.format("%s PLACES %s %d:%d %s", id, params[0], params[1], params[2], params[3]);
+    }
+
+    public void execute(List<String> parts, V view){
+        String[] xy = parts.get(3).split(":");
+        int x = Integer.parseInt(xy[0]);
+        int y = Integer.parseInt(xy[1]);
+        String meeple = parts.get(4);
+        view.updateOnPlace(parts.get(0), null /*TODO à faire quand les tiles sont dispo*/, x, y, meeple);
+    }
+}
