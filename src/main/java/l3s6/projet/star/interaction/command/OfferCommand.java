b024fdee026c788cdb10b7bcac0cefb259d7c7fb
Andrieu Remi <remi.andrieu.etu@a13p14.fil.univ-lille.fr>
2026-02-04 11:16:32 +0100

    test: OfferCommandTest

--- src/main/java/l3s6/projet/star/interaction/command/OfferCommand.java
+++ src/main/java/l3s6/projet/star/interaction/command/OfferCommand.java
@@ -11,6 +11,10 @@ public class OfferCommand<V extends AbstractView> extends AbstractCommand<V> {
     }
 
     public String build(String id, Object... params) throws InvalidArgumentNumberException {
+        /* format: id OFFERS tile id+ */
+        if (params == null || params.length < 2){
+            throw new InvalidArgumentNumberException("Invalid number of arguments given (must be 2 or more for OFFERS command)");
+        }
         StringBuilder stringBuilder = new StringBuilder(id).append(" ").append(this.keyword);
         for (Object o : params){
             stringBuilder.append(" ").append(o);
