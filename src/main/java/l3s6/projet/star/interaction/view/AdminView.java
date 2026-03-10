2ded0723be0cfed5cf1ce8f39205c3c2085e67d5
Rémi Andrieu <remi.andrieu@outlook.fr>
2026-03-10 20:12:23 +0100

    feat: simple implementation, spectators receive messages, players and admins can also sends them, all through the command line. TODO : parse input to call the correct method of client

--- src/main/java/l3s6/projet/star/interaction/view/AdminView.java
+++ src/main/java/l3s6/projet/star/interaction/view/AdminView.java
@@ -8,7 +8,7 @@ import l3s6.projet.star.interaction.router.AdminRouter;
 import java.net.URISyntaxException;
 import java.util.List;
 
-public abstract class AdminView<T extends AdminClient> extends PlayerView<T> {
+public class AdminView<T extends AdminClient> extends PlayerView<T> {
 
     public AdminView(String ipAddress, int port, String id) throws URISyntaxException, InterruptedException{
         super(ipAddress, port, id);
@@ -22,6 +22,8 @@ public abstract class AdminView<T extends AdminClient> extends PlayerView<T> {
         return new AdminClient(ipAddress, port, id, this.dispatcher);
     }
 
-    public abstract void updateOnGrant(String id, String grantedPlayer, List<String> keywords);
+    public void updateOnGrant(String id, String grantedPlayer, List<String> keywords) {
+        System.out.println(String.format("[%s] Player %s was granted the keywords %s.", id, grantedPlayer, keywords.toString()));
+    }
 
 }
--- src/main/java/l3s6/projet/star/interaction/view/PlayerView.java
+++ src/main/java/l3s6/projet/star/interaction/view/PlayerView.java
@@ -1,13 +1,14 @@
 package l3s6.projet.star.interaction.view;
 
 import java.net.URISyntaxException;
+import java.util.Scanner;
 
 import l3s6.projet.star.interaction.network.AbstractClient;
 import l3s6.projet.star.interaction.network.PlayerClient;
 import l3s6.projet.star.interaction.router.GameListener;
 import l3s6.projet.star.interaction.router.PlayerRouter;
     
-public abstract class PlayerView<T extends PlayerClient> extends SpectatorView<T> {
+public class PlayerView<T extends PlayerClient> extends SpectatorView<T> {
 
     public PlayerView(String ipAddress, int port, String id) throws URISyntaxException, InterruptedException{
         super(ipAddress, port, id);
@@ -23,6 +24,35 @@ public abstract class PlayerView<T extends PlayerClient> extends SpectatorView<T
 
     protected AbstractClient createClient(String ipAddress, int port, String id) throws URISyntaxException, InterruptedException {
         return new PlayerClient(ipAddress, port, id, this.dispatcher);
+    }
+
+    public static void main(String[] args) {
+        String host = "127.0.0.1";
+        int port = 5000;
+        String playerID = "Joueur1";
+
+        try {
+            PlayerView<?> view = new PlayerView<>(host, port, playerID);
+
+            Scanner scanner = new Scanner(System.in);
+            boolean running = true;
+
+            while (running) {
+                String input = scanner.nextLine();
+
+                if (input.equalsIgnoreCase("quit")) {
+                    running = false;
+                } else {
+                    view.client.place("Joueur1", "Wf-f-f-f", 1, 2); 
+                }
+            }
+
+            scanner.close();
+            System.exit(0);
+
+        } catch (URISyntaxException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
