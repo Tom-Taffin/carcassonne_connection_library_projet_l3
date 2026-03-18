27ecede0061698adb7cd5116cfa22249b3c5f788
Samuel DEMON <samuel.demon.etu@univ-lille.fr>
2026-03-18 19:56:27 +0100

    docs: InvalidArgumentNumberException

--- src/main/java/l3s6/projet/star/interaction/command/InvalidArgumentNumberException.java
+++ src/main/java/l3s6/projet/star/interaction/command/InvalidArgumentNumberException.java
@@ -1,6 +1,14 @@
 package l3s6.projet.star.interaction.command;
 
+/**
+ * Exception thrown when a command receives an invalid number of arguments.
+ */
 public class InvalidArgumentNumberException extends Exception {
+
+    /**
+     * Constructs an InvalidArgumentNumberException with the specified detail message.
+     * @param message the detail message describing the cause of the exception
+     */
     public InvalidArgumentNumberException(String message) {
         super(message);
     }
