8ca738d5b5836476e29b48e712e65aae6bc9804c
Samuel DEMON <samuel.demon.etu@univ-lille.fr>
2026-03-19 09:29:20 +0100

    docs: PlayCommand

--- src/main/java/l3s6/projet/star/interaction/command/PlayCommand.java
+++ src/main/java/l3s6/projet/star/interaction/command/PlayCommand.java
@@ -4,12 +4,28 @@ import l3s6.projet.star.interaction.view.SpectatorView;
 
 import java.util.List;
 
+/**
+ * Command representing a play action in the game's interaction system.
+ * This command is identified by the keyword "PLAYS" and follows the format: id PLAYS.
+ * Upon reception, it triggers a view update via the updateOnPlay method.
+ * @param <V> the type of spectator view this command operates on, must extend SpectatorView
+ */
 public class PlayCommand<V extends SpectatorView<?>> extends AbstractCommand<V> {
 
+    /**
+     * Constructs a PlayCommand and sets its keyword to "PLAYS".
+     */
     public PlayCommand(){
         this.keyword = "PLAYS";
     }
 
+    /**
+     * Builds the string representation of the PLAYS command to be sent over the network.
+     * @param id the identifier of the entity issuing the play
+     * @param params must be null or empty, as the PLAYS command takes no parameters
+     * @return the formatted PLAYS command string ready to be transmitted
+     * @throws InvalidArgumentNumberException if params is non-null and contains at least one element
+     */
      public String build(String id, Object... params) throws InvalidArgumentNumberException {
         if (params != null && params.length != 0){
             throw new InvalidArgumentNumberException("Invalid number of arguments given (must be 0 for " + this.keyword + " command)");
@@ -17,6 +33,13 @@ public class PlayCommand<V extends SpectatorView<?>> extends AbstractCommand<V> 
         return id + " " + this.keyword;
      }
 
+    /**
+     * Executes this command on the receiving end, applying its effect on the given view.
+     * @param id the identifier of the entity targeted by the command
+     * @param parts the list of string tokens parsed from the received command message, must be empty
+     * @param view the spectator view to update or interact with as a result of the command
+     * @throws InvalidArgumentNumberException if parts is not empty
+     */
      public void execute(String id, List<String> parts, V view) throws InvalidArgumentNumberException {
          if (!parts.isEmpty()){
              throw new InvalidArgumentNumberException("Invalid number of arguments received (must be 0 for " + this.keyword + " command)");
