d5bb23233b812d6fa8005cbb4028691faca039a0
Samuel DEMON <samuel.demon.etu@univ-lille.fr>
2026-03-19 09:43:15 +0100

    docs: SpectatorClient constructor

--- src/main/java/l3s6/projet/star/interaction/network/SpectatorClient.java
+++ src/main/java/l3s6/projet/star/interaction/network/SpectatorClient.java
@@ -9,6 +9,14 @@ import l3s6.projet.star.interaction.router.GameListener;
  */
 public class SpectatorClient extends AbstractClient {
 
+    /**
+     * A spectator connect the same way as {@link AbstractClient}
+     * @param ip the IP address of the server
+     * @param port the port of the server
+     * @param updateListener the {@link l3s6.projet.star.interaction.router.GameListener} that will be updated when a message is received
+     * @throws URISyntaxException if the server URI has incorrect syntax
+     * @throws InterruptedException if the connection is interrupted
+     */
     public SpectatorClient(String ip, int port, GameListener updateListener) throws URISyntaxException, InterruptedException{
         super(ip, port, updateListener);
     }
